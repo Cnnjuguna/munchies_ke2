6cab98986b68de8a09bc02c220550ff8a921e3a2
Cnnjuguna <123736824+Cnnjuguna@users.noreply.github.com>
2023-09-10 21:18:44 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import './App.css';
+import Pages from './pages/Pages';
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hello</h1>
+        <Pages />
+      </div>
+    </>
+  );
+}
+
+export default App;
